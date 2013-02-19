4769a48b9c49040d71812f40cda9ca50fac0d2a2
Smart Viking <smartviking@smartviking.(none)>
2013-02-19 19:32:09 +0100

    Putting code into more appropriate places yo

--- window.js
+++ window.js
@@ -1,26 +1,9 @@
-var no_user_select = {
-    "-webkit-touch-callout": "none",
-    "-webkit-user-select": "none",
-    "-khtml-user-select": "none",
-    "-moz-user-select": "none",
-    "-ms-user-select": "none",
-    "user-select": "none"}
-
-var no_user_select_disable = {
-    "-webkit-touch-callout": "",
-    "-webkit-user-select": "",
-    "-khtml-user-select": "",
-    "-moz-user-select": "",
-    "-ms-user-select": "",
-    "user-select": ""}
-
-
 $(document).ready( function() {
     $(".close-window-button").on('mousedown', function(e) {
 	var window = $(this).parent().parent();
 	window.css("display","none");
     });
-    $(".jswindow > .window-top").on('mousedown', function(e) {
+    $(".jswindow > .window-top").click( function(e) {
 	var window = $(this).parent();
 	var jswinID = window.attr("id");
 
@@ -29,23 +12,23 @@ $(document).ready( function() {
 			   'left':e.pageX - of.left};
 	$(document).on('mousemove.move', function(e) {
 	    window.css({"top" : e.pageY - clickoffset.top,
-					  "left": e.pageX - clickoffset.left});
+			"left": e.pageX - clickoffset.left});
 	});
 	$(document).on('mouseup', function(e) {
 	    $(this).off('mousemove.move');
 	    $(this).off('mouseup');
 	});
     });
+
     $(".jswindow").on('mousedown', function (e) {
-	$(this).css("z-index", 
-		    String(parseInt($(this).css("z-index")) + 2));
+	$(this).css("z-index", String(parseInt($(this).css("z-index")) + 2));
     });
+
     $(".jswindow > .resize-window").on("mousedown", function(e) {
 	var window = $(this).parent();
 	var resisor = $(this);
 	var of = window.offset();
-
-	window.css(no_user_select);
+	window.addClass("no-user-select");
 
 	$(document).on('mousemove.resize', function(e) {
 	    var clickoffset = {'top': e.pageY - of.top,
@@ -59,13 +42,12 @@ $(document).ready( function() {
 	});
 
 	$(document).on('mouseup', function(e) {
-	    window.css(no_user_select_disable);
+	    window.removeClass("no-user-select");
 	    $(this).off('mousemove.resize');
 	    $(this).off('mouseup');
 	});
-
     });
 
-    $(".resize-window").css(no_user_select);
-    $(".window-title").css(no_user_select);
+    $(".resize-window").addClass("no-user-select");
+    $(".window-title").addClass("no-user-select");
 });
