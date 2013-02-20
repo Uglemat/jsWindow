085663a3d4b76127b4cb591e8fa5edd410e9805b
Smart Viking <smartviking@smartviking.(none)>
2013-02-20 12:14:38 +0100

    Breaking stuff

--- window.js
+++ window.js
@@ -1,49 +1,78 @@
+$(document).ready( function () {
+    var buildWindow = function () {
+	win_str = "<div class='jswindow' id='1' style='z-index:1;'>"
 
-$(".close-window-button").on('mouseup', function(e) {
-    var window = $(this).parent().parent();
-    window.css("display","none");
-});
-$(".jswindow > .window-top").click( function(e) {
-    var window = $(this).parent();
-    var jswinID = window.attr("id");
+    }
 
-    var of = window.offset()
-    var clickoffset = {'top': e.pageY - of.top,
-		       'left':e.pageX - of.left};
-    $(document).on('mousemove.move', function(e) {
-	window.css({"top" : e.pageY - clickoffset.top,
-		    "left": e.pageX - clickoffset.left});
+
+    $(".close-window-button").mouseup( function(e) {
+	var win = $(this).parent().parent();
+	win.css("display","none");
     });
-    $(document).on('mouseup', function(e) {
-	$(this).off('mousemove.move');
-	$(this).off('mouseup');
-    });
-});
+    $(".jswindow > .window-top").mousedown( function(e) {
+	var win = $(this).parent();
+	var jswinID = win.attr("id");
 
-$(".jswindow").on('mousedown', function (e) {
-    $(this).css("z-index", String(parseInt($(this).css("z-index")) + 2));
-});
-
-$(".jswindow > .resize-window").click( function(e) {
-    var window = $(this).parent();
-    var resisor = $(this);
-    var of = window.offset();
-    window.addClass("no-user-select");
-
-    $(document).on('mousemove.resize', function(e) {
+	var of = win.offset()
 	var clickoffset = {'top': e.pageY - of.top,
 			   'left':e.pageX - of.left};
-	var top_height = window.children(".window-top").height();
-	var w = Math.max(150,clickoffset.left)
-	var h = Math.max(30, clickoffset.top - top_height - resisor.height())
-
-	window.css("width",w+5);
-	window.css("height",h);
+	$(document).on('mousemove.move', function(e) {
+	    win.css({"top" : e.pageY - clickoffset.top,
+		     "left": e.pageX - clickoffset.left});
+	});
+	$(document).on('mouseup', function(e) {
+	    $(this).off('mousemove.move');
+	    $(this).off('mouseup');
+	});
     });
 
-    $(document).on('mouseup', function(e) {
-	window.removeClass("no-user-select");
-	$(this).off('mousemove.resize');
-	$(this).off('mouseup');
+    $(".jswindow").mousedown( function (e) {
+	$(this).css("z-index", String(parseInt($(this).css("z-index")) + 2));
+	var win = $(this);
+
+	$(document).on( "mousemove.debuginfo", function(e) {
+
+	    var of = win.offset();
+	    var s = "";
+	    s += "<br>e.pageY : " + e.pageY;
+	    s += "<br>e.pageX : " + e.pageX;
+	    s += "<br><br>win.offset().top : " + of.top;
+	    s += "<br>win.offset().left : " + of.left;
+
+	    s += "<br><br>win.outerWidth() : " + win.outerWidth();
+	    s += "<br>win.outerHeight() : " + win.outerHeight();
+	    $("#dbinfo").html(s);
+	});
     });
 
+    $(".jswindow .resize-window").mousedown( function(e) {
+	$("*").addClass("no-user-select")
+	var win = $(this).parent();
+	var cont_cont = win.children(".window-content-container");
+	var win_top = win.children(".window-top");
+	var of = win.offset();
+
+	var winwidth = win.outerWidth()
+	var winheight = win.outerHeight()
+	var lco = winwidth - (e.pageX - of.left) 
+	var tco = winheight - (e.pageY - of.top)
+
+	$(document).on('mousemove.resize', function(e) {
+
+	    var h = Math.max(100, e.pageY - of.top + tco)
+	    var w = Math.max(150, e.pageX - of.left + lco)
+
+	    win.css("width",w);
+	    win.css("height",h);
+
+	    cont_cont.css("height",h-win_top.outerHeight()-16)
+	});
+
+	$(document).mouseup( function(e) {
+	    $("*").removeClass("no-user-select")
+	    $(this).off('mousemove.resize');
+	    $(this).off('mouseup');
+	});
+    });
+
+});
