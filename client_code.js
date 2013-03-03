b8937124ead038323bf308395cb8464afbf697ca
Smart Viking <smartviking@smartviking.(none)>
2013-03-03 23:17:24 +0100

    New theme: windows7

--- client_code.js
+++ client_code.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
     var wg = new jsWindow.windowGroup($("#windows"));
-    wg.appendWindow({title: "My title",theme:"gnome3"});
+    wg.appendWindow({title: "My title AYAYAY",theme:"softblue"});
+    wg.appendWindow({title: "My title AYssssAYAY",theme:"gnome3"});
+    wg.appendWindow({title: "My title AYssssAYAY",theme:"windows7"});
 });
