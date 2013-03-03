6bffc4ef23904e777def58c5f31e4bd74be13318
Smart Viking <smartviking@smartviking.(none)>
2013-03-03 02:55:51 +0100

    New theme: gnome3

--- client_code.js
+++ client_code.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     var wikipedia_iframe = 
-            "<iframe src='http://www.wikipedia.com' style='width:480px;height:320px'>" +
+            "<iframe src='http://www.wikipedia.com' style='width:470px;height:320px'>" +
             "  <p>Your browser does not support iframes.</p>   " +
             "</iframe>";
     
     var wg = new jsWindow.windowGroup($("#windows"), {
         keep_windows_on_page: {bottom:true, right:true, left:true},
         opaque_when_moving: true,
-        theme: "softblue"
+        theme: "gnome3"
     });
     wg.appendWindow({title: "11111 uihouyo ghuoiygiytgi ytfityf iytf111111",
                      id: "1",top:200,left:200, theme: "plain"});
@@ -15,13 +15,14 @@ $(document).ready(function () {
                      top: 300, left: 100, width: 400, height: 200});
     var wpID = wg.appendWindow(
         { id: "3",
+          theme: "gnome3",
           content: wikipedia_iframe + "<input type=button id=close value='Close window'>",
           height: 400,
           width: 500,
           title: "333333333333",
           left: 400,
-          close_button: false,
-          resizable: false });
+          close_button: true,
+          resizable: true });
     
 
     $("#close").click(function() {
