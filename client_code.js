1c5e8ebb069d87cd5ad8dbda0faa6bbf63f4245f
Smart Viking <smartviking@smartviking.(none)>
2013-03-03 16:06:43 +0100

    Formatted some gradient CSS stuff so it doesn't look so ugly

--- client_code.js
+++ client_code.js
@@ -28,4 +28,26 @@ $(document).ready(function () {
     $("#close").click(function() {
         wg.remove_window(wpID);
     });
+
+    var owg = new jsWindow.windowGroup($("#some_other_windows"), {
+        keep_windows_on_page: {bottom:true, right:true, left:true},
+        opaque_when_moving: true,
+        theme: "gnome3",
+        start_z_index: 300
+    });
+    owg.appendWindow({title: "11111 uihouyo ghuoiygiytgi ytfityf iytf111111",
+                     id: "1ww",top:200,left:200, theme: "plain"});
+    owg.appendWindow({title: "22222222222",id: "s2", 
+                     top: 300, left: 100, width: 400, height: 200});
+    owg.appendWindow(
+        { id: "3dd",
+          theme: "softblue",
+          content: wikipedia_iframe,
+          height: 400,
+          width: 500,
+          title: "333333333333",
+          left: 400,
+          close_button: true,
+          resizable: true });
+
 });
