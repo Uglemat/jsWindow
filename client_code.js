fc8846981ea8953f5d8bccf65afb56714edd8b1a
Smart Viking <smartviking@smartviking.(none)>
2013-03-04 05:41:19 +0100

    p.window-title has min-height: 20px;, which will cause things to work correctly if the title is the empty string (which obviously can be useful).

--- client_code.js
+++ client_code.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var wg = new jsWindow.windowGroup($("#windows"),{shadow: false });
     wg.appendWindow({title: "My title AYssssAYAY",theme:"gnome3", shadow: true});
-    wg.appendWindow({title: "My title AYssssAYAY",theme:"windows7", shadow: true});
-    wg.appendWindow({title: "PLAAAIN"});
+    wg.appendWindow({title: "",theme:"windows7", shadow: true});
+    wg.appendWindow({title: ""});
     wg.appendWindow({title: "My title AYAYAY",theme:"softblue",top:40,left:40});
 });
