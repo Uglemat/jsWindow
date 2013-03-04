34ebace75fef3830d0883264be2d8bcc77a7b970
Smart Viking <smartviking@smartviking.(none)>
2013-03-04 17:50:10 +0100

    Red-ish shadows for the windows7 theme close button

--- client_code.js
+++ client_code.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     var wg = new jsWindow.windowGroup($("#windows"),{shadow: false });
     wg.appendWindow({title: "My title AYssssAYAY",theme:"gnome3", shadow: true});
-    wg.appendWindow({title: "",theme:"windows7", shadow: true});
+    wg.appendWindow({title: "OMFGGGGG AAR PURR PURR",theme:"windows7", shadow: true,height:300,top:200});
     wg.appendWindow({title: ""});
+    wg.appendWindow({title: "HALLALAL"});
     wg.appendWindow({title: "My title AYAYAY",theme:"softblue",top:40,left:40});
+    wg.appendWindow({title: "",theme:"windows7", shadow: true});
 });
